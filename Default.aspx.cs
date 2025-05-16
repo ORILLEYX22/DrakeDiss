f04b4193d8523b407b6ee0032ce617c5129223d9
ORILLEYX22 <mbathaseneleorilleyx@gmail.com>
2025-05-16 21:30:18 +0200

    Create Default.aspx.cs

---
+++ Default.aspx.cs
@@ -0,0 +1,11 @@
+using System;
+namespace SimpleWebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // You can add server-side logic here if needed.
+        }
+    }
+}
